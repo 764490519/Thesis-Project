0584867b0556b81719718df8eca8b462f43c96d6
764490519 <liangguoqing777@gmail.com>
2025-02-05 19:33:59 +0800

    Extract HeartRate as a separate Activity.

--- app/src/main/java/com/example/datacollector/presentation/HeartRateActivity.kt
+++ app/src/main/java/com/example/datacollector/presentation/HeartRateActivity.kt
@@ -1,14 +1,11 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
- * most up to date changes to the libraries and their usages.
- */
-
 package com.example.datacollector.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +14,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.health.services.client.HealthServices
 import androidx.health.services.client.MeasureCallback
 import androidx.health.services.client.MeasureClient
 import androidx.health.services.client.data.Availability
@@ -24,18 +24,16 @@ import androidx.health.services.client.data.DataPointContainer
 import androidx.health.services.client.data.DataType
 import androidx.health.services.client.data.DataTypeAvailability
 import androidx.health.services.client.data.DeltaDataType
+import androidx.lifecycle.lifecycleScope
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.example.datacollector.presentation.theme.DataCollectorTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import android.content.Intent
-import android.graphics.Color
-import android.view.Gravity
-import android.widget.Button
-import android.widget.LinearLayout
+import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.launch
 
-class MainActivity : ComponentActivity() {
+class HeartRateActivity : ComponentActivity(){
 
     private lateinit var measureClient: MeasureClient
 
@@ -65,82 +63,41 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setTheme(android.R.style.Theme_DeviceDefault)
-
-
-        Log.i("MainActivityUserDebug","MainActivity onCreate")
-
-        val rootLayout = LinearLayout(this)
-        rootLayout.orientation = LinearLayout.VERTICAL
-        rootLayout.gravity = Gravity.CENTER
-
-        val bleScanButton = Button(this)
-        bleScanButton.text = "scan BLE devices"
-        bleScanButton.setTextColor(Color.BLUE)
-
-
-        val heartrateButton = Button(this)
-        heartrateButton.text = "measure HeartRate"
-        heartrateButton.setTextColor(Color.RED)
-
-        rootLayout.addView(bleScanButton)
-        rootLayout.addView(heartrateButton)
-
-        setContentView(rootLayout)
-
-        bleScanButton.setOnClickListener {
-            Log.i("MainActivityUserDebug","BLE Button Clicked")
-            bleScanButton.text = "CLICKED!!!"
-            val intent = Intent(this, BLEScanActivity::class.java)
-            startActivity(intent)
-        }
-
-        heartrateButton.setOnClickListener {
-            Log.i("MainActivityUserDebug","HeartRate Button Clicked")
-            heartrateButton.text = "CLICKED!!!"
-            val intent = Intent(this, HeartRateActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
-
+        Log.i("HeartRateActivityUserDebug", "HeartRateActivity onCreate")
 
 //        check Permission
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS)
-//            != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            // Request Permission
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(Manifest.permission.BODY_SENSORS),
-//                1001
-//            )
-//        }
-//
-//
-//        val healthClient = HealthServices.getClient(this /*context*/)
-//        measureClient = healthClient.measureClient
-//
-//
-//        var supportsHeartRate = false;
-//
-//        Log.d("TEST", "TEST...........")
-//
-//        lifecycleScope.launch {
-//            val capabilities = measureClient.getCapabilitiesAsync().await()
-//            supportsHeartRate = DataType.HEART_RATE_BPM in capabilities.supportedDataTypesMeasure
-//
-//            measureClient.registerMeasureCallback(
-//                DataType.Companion.HEART_RATE_BPM,
-//                heartRateCallback
-//            )
-//
-//            setContent {
-//                WearApp(supportsHeartRate, heartRateFlow)
-//            }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request Permission
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BODY_SENSORS),
+                1001
+            )
+        }
+
+        val healthClient = HealthServices.getClient(this /*context*/)
+        measureClient = healthClient.measureClient
+
+
+        var supportsHeartRate = false;
+
+        Log.d("TEST", "TEST...........")
+
+        lifecycleScope.launch {
+            val capabilities = measureClient.getCapabilitiesAsync().await()
+            supportsHeartRate = DataType.HEART_RATE_BPM in capabilities.supportedDataTypesMeasure
+
+            measureClient.registerMeasureCallback(
+                DataType.Companion.HEART_RATE_BPM,
+                heartRateCallback
+            )
+
+            setContent {
+                WearApp(supportsHeartRate, heartRateFlow)
+            }
         }
     }
 
@@ -169,10 +126,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-
-//@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
-//@Composable
-//fun DefaultPreview() {
-//    WearApp(false)
-//}
+}
